689a2f8e9e83d459b47b75500ce0a8df008a059d
diegolins10 <diegofernandeslins@gmail.com>
2021-07-14 20:45:41 -0300

    Super class Account

---
+++ Business/Entities/Account.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Entities
+{
+    /******** SUPER CLASSE ***********/
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; protected set; } // pode ser alterado nas subclasse da heranca
+
+        public Account()
+        {
+
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Withdraw(double amount) //sacar
+        {
+            Balance -= amount;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+    }
+}
