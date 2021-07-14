97f1ef640377d245b5e0906cd4890d8457419d13
diegolins10 <diegofernandeslins@gmail.com>
2021-07-14 20:46:05 -0300

    sub class Business account

---
+++ Business/Entities/BusinessAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Entities
+{
+    /******** SUB CLASSE ***********/
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        BusinessAccount()
+        {
+
+        }
+
+        /******** USANDO O REAPROVEITAMENTO DE CONSTRUTOR COM BASE ***********/
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
