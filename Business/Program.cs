d9794dcf44e1295922b0f71a46b658f47bc71e56
diegolins10 <diegofernandeslins@gmail.com>
2021-07-15 20:46:46 -0300

    polimorfismo

--- Business/Program.cs
+++ Business/Program.cs
@@ -8,14 +8,16 @@ namespace Business
         static void Main(string[] args)
         {
 
-            Account account = new Account(02, "tais", 500);
+            //Polimorfismo variaveis do mesmo tipo instanciados com objetos difrentes terao comportamento diferente.
+            Account ac2 = new Account(02, "tais", 500);
             Account ac1 = new SavingsAccount(01, "diego", 500, 0.5);
             //account.Balance = 10;
-            account.Withdraw(10);
+            ac2.Withdraw(10);
             ac1.Withdraw(10);
 
+            Console.WriteLine(ac2.Balance);
             Console.WriteLine(ac1.Balance);
-            Console.WriteLine(account.Balance);
+            
         }
     }
 }
